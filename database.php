20e939720f354246b9312ef0d1894e3ada9404f2
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-12-08 16:08:40 -0500

    database

--- database.php
+++ database.php
@@ -1,6 +1,7 @@
 <?php
 
-define('DATABASE', 'kwilliam');
-define('USERNAME', 'kwilliam');
-define('PASSWORD', 'USracpEto');
-define('CONNECTION', 'sql2.njit.edu');
+define('DATABASE', 'sm2555');
+define('USERNAME', 'sm2555');
+define('PASSWORD', 'tzw8bjLL');
+define('CONNECTION', 'sql1.njit.edu');
+?>
