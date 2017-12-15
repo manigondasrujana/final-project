eff432207eaa848904a52de5ac867c38347ae7e6
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-12-15 13:11:53 -0500

    database

--- database.php
+++ database.php
@@ -1,6 +1,36 @@
 <?php
 
-define('DATABASE', 'kwilliam');
-define('USERNAME', 'kwilliam');
-define('PASSWORD', 'USracpEto');
-define('CONNECTION', 'sql2.njit.edu');
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+define('DATABASE', 'sm2555');
+define('USERNAME', 'sm2555');
+define('PASSWORD', 'tzw8bjLL');
+define('CONNECTION', 'sql1.njit.edu');
+
+class dbConn
+{
+       protected static $db;
+       private function __construct() 
+        {
+           try 
+             {
+              self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
+              self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+             }
+           catch (PDOException $e)
+             {
+              echo "Connection Error: " . $e->getMessage();
+             }
+        }
+      public static function getConnection() 
+        {
+            if (!self::$db) 
+            {
+                 new dbConn();
+            }
+       
+         return self::$db;
+        }
+}
+?>
