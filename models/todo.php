c6b7ab04a93d941bf62219d6684f955413bf412d
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-12-14 19:16:20 -0500

    todo

--- models/todo.php
+++ models/todo.php
@@ -3,17 +3,17 @@
 final class todo extends database\model
 {
     public $id;
-    public $owneremail;
+    //public $owneremail;
     public $ownerid;
     public $createddate;
-    public $duedate;
-    public $message;
+	  public $updateddate;
+    // public $duedate;
+    //public $message;
     public $isdone;
     protected static $modelName = 'todo';
 
     public static function getTablename()
     {
-
         $tableName = 'todos';
         return $tableName;
     }
