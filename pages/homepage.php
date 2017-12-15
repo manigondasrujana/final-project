c189944bc87a4481d658f1e8f85fba396ed3b453
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-12-15 01:05:02 -0500

    homepage

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,11 +1,10 @@
 <!doctype html>
-
 <html lang="en">
 <head>
     <meta charset="utf-8">
 
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
+    <title>Todo Task Management</title>
+    <meta name="description" content="Todo Task Management">
     <meta name="author" content="SitePoint">
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
@@ -13,39 +12,32 @@
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
     <![endif]-->
+    <style>
+    body {background-color: powderblue;}
+h1   {color: blue;}
+h2    {color: red;}
+</style>
 </head>
 
-<body>
-
-
+<body> 
 <h1>
-    <?php
+<?php
+$config = Manage::config();
+echo $config['site_name'];
+?></h1>
 
-    //this how to print some data;
-    echo $data['site_name'];
-
-    ?> </h1>
-
-<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
-<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
-
+<h2> User Login</h2> 
 <form action="index.php?page=accounts&action=login" method="POST">
-
     <div class="container">
-        <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="email" required>
-
-        <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
-
+        <label><b>  Username  </b></label>
+        <input type="text" placeholder=" Enter Username " name="email" required></br></br>
+        <label><b>  Password  </b></label>
+        <input type="password" placeholder=" Enter Password " name="password" required></br></br>
         <button type="submit">Login</button>
     </div>
-
-
 </form>
-<h1><a href="index.php?page=accounts&action=register">Register</a></h1>
 
-
+<h1><a href="index.php?page=accounts&action=register"> Create an Account </a></h1>
 <script src="js/scripts.js"></script>
 </body>
 </html>
