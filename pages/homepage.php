5f0118786d77d42e7edafb8d4df3e7caa3906f58
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-12-15 15:31:14 -0500

    homepage

--- pages/homepage.php
+++ pages/homepage.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
 
-    <title>Todo Task Management Application</title>
+    <title >Todo Task Management Application</title>
     <meta name="description" content="Todo Task Management Application">
     <meta name="author" content="SitePoint">
 
@@ -15,34 +15,34 @@
     <style>
     body {
    
-    background-image: url(http://i65.tinypic.com/2vkg9dd.jpg), url(http://i68.tinypic.com/s0vcqg.jpg);
-    background-position:left top, right top;
+    background-image: url(http://i66.tinypic.com/155taxe.jpg), url(http://i68.tinypic.com/s0vcqg.jpg);
+    background-position:center, right top;
 background-repeat: no-repeat, repeat;
-background-size: 550px,450px, auto}
+background-size: 800px,600px, auto}
 h1   {color: blue;}
 h2    {color: red;}
 </style>
 </head>
 
-<body> 
+<body style="text-align:center;"> 
 <h1>
 <?php
 $config = Manage::config();
 echo $config['site_name'];
 ?></h1>
 
-<h2> User Login</h2> 
+<h2 style="text-align:center;" style="color:crimson"> User Login</h2> 
 <form action="index.php?page=accounts&action=login" method="POST">
-    <div class="container">
-        <label><b>  Username  </b></label>
+    <div style="text-align:center;" class="container">
+        <label style="color:darkgreen"><b>  Username  </b></label>
         <input type="text" placeholder=" Enter Username " name="email" required></br></br>
-        <label><b>  Password  </b></label>
+        <label style="color:darkgreen"><b>  Password  </b></label>
         <input type="password" placeholder=" Enter Password " name="password" required></br></br>
         <button type="submit">Login</button>
     </div>
 </form>
 
-<h1><a href="index.php?page=accounts&action=register"> Create an Account </a></h1>
+<h1  style="text-align:center"><a href="index.php?page=accounts&action=register"  style="color:crimson"> Create an Account </a></h1>
 <script src="js/scripts.js"></script>
 </body>
 </html>
