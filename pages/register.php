2e0b706107553490d7c6c9c2037cf7cbdef1aad4
Srujana Manigonda <sm2555@afsconnect1.njit.edu>
2017-12-15 01:29:13 -0500

    register

--- pages/register.php
+++ pages/register.php
@@ -3,32 +3,33 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
+    <title>Todo Task Management</title>
+    <meta name="description" content="Todo Task Management">
     <meta name="author" content="SitePoint">
-
     <link rel="stylesheet" href="css/styles.css?v=1.0">
-
     <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
     <![endif]-->
 </head>
 
 <body>
+<h1>
+<?php
+$config = Manage::config();
+echo $config['site_name'];
+?></h1>
 
-
+<h2> Create an Account</h2>
 <form action="index.php?page=accounts&action=register" method="post">
-    First name: <input type="text" name="fname"><br>
-    Last name: <input type="text" name="lname"><br>
-    Email: <input type="text" name="email"><br>
-    Phone: <input type="text" name="phone"><br>
-    Birthday: <input type="text" name="birthday"><br>
-    Gender: <input type="text" name="gender"><br>
-    Password: <input type="password" name="password"><br>
+    First name: <input type="text" name="fname" placeholder="first name"required><br>
+    Last name: <input type="text" name="lname"placeholder="lastname" required><br>
+    Email or username: <input type="email" name="email" placeholder="username"required><br>
+    Phone: <input type="tel" name="phone"placeholder="phone no" required><br>
+    Birthday: <input type="text" name="birthday" placeholder="yyyy-mm-dd" required><br>
+    Gender: <input type="radio" name="gender" value="male" checked="checked">Male <input type="radio" name="gender" value="female"> Female<br>
+    Password: <input type="password" name="password" required><br>
     <input type="submit" value="Submit form">
 </form>
-
 
 <script src="js/scripts.js"></script>
 </body>
